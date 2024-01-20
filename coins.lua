90b8e7ae1c0637f4493d35b67a1163c0bf31297f
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-01-20 17:44:09 +0500

    Create coins.lua

---
+++ coins.lua
@@ -0,0 +1,29 @@
+--[[
+
+Please note that this is important,, dont remove any Text(s) from here
+
+]]--
+
+return(function()local A ='mJjs+dw==','edeEDEds==','EdjJIWg==' B>D<J while a do true if a then do true (2342-3245),(242+244),(334-212),(422+384),(34-67),'aBxc','werf','erferf','wefwdaE'  return coins
+
+      --Data
+
+      local userData = {}
+         tostring()
+           GetUserPlatformerID()
+             tostring()
+              UserTokens=(UserTokens)
+    else
+      UserTokens = if 0 
+        tostring()
+          UserTokens(0)
+          local numbers '1234567890'
+           use()
+             tostring()
+           numbers
+        tostring()
+         toCheckValue
+        --ends
+
+            Line7 = end
+          Line11,12,13,14,15,16,17,18,19,20,21,22,23,24,25 = end 
