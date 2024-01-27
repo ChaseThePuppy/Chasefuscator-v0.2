4bc00159525abd600ef61c02bc8d31df982d78ee
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-01-27 10:20:51 +0500

    Create main.lua

---
+++ g2048235/main.lua
@@ -0,0 +1,18 @@
+    "The file is not found",
+    "the file is found",
+    "the lua version is not 5.1 its higher than it",
+    "storage/emulated/0/Android/data/com.sandboxol.blockymods/files/download/blockman/map/media/scripts/engine/lua/",
+    os.add,
+    "..commom.zip..",
+    "..ic..",
+    "..sandboxol..",
+    os.remove,
+    "..GMHelper.lua..",
+    then os.add
+    "..GMMod.lua..",
+    then os.rename
+    "..GMMod.lua..",
+    to(os.rename)
+    "... GMHelper.lua ..."
+--end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end end 
+end)
