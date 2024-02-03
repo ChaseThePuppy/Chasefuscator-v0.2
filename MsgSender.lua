b073a10b7743fee53d96ef18a17371d2d5062d20
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-02-03 08:45:08 +0500

    Create MsgSender.lua

---
+++ MsgSender.lua
@@ -0,0 +1,12 @@
+--[[
+
+╔═══╦╗───────────╔═╗───────────╔╗
+║╔═╗║║───────────║╔╝──────────╔╝╚╗
+║║─╚╣╚═╦══╦══╦══╦╝╚╦╗╔╦══╦══╦═╩╗╔╬══╦═╗
+║║─╔╣╔╗║╔╗║══╣║═╬╗╔╣║║║══╣╔═╣╔╗║║║╔╗║╔╝
+https://discord.gg/umuj2jPx5T
+║╚═╝║║║║╔╗╠══║║═╣║║║╚╝╠══║╚═╣╔╗║╚╣╚╝║║
+╚═══╩╝╚╩╝╚╩══╩══╝╚╝╚══╩══╩══╩╝╚╩═╩══╩╝
+]]--
+
+local v0=bit32 or bit ;local v1=v0['bxor'];local v2=v0['bor'];local v3=v0['band'];local v4="Clicking [Strings] will completely hide this string!";local v5=1 + 0 ;local v6=8023524 -(35 + 8) ;local v7=3375 -2378 ;local v8=1385 -(97 + 1169) ;local v9=1091 -(68 + 997) ;local v10=192222 -(226 + 1044) ;local v11=1194830 -920434 ;local v12=364218 -(32 + 85) ;local v13=15841 + 323 ;local v11=v3(v13 + (((v3(5080 -1216 ,v12) + v2(857 + 3007 ,v12)) -v11) -(68557 -(892 + 65))) ,v10-(263930 -153292) ) + v2(v13 + (((v3(8451 -4587 ,v12) + v2(672 + 3192 ,v12)) -v11) -(120111 -52511)) ,v10-110638 ) ;v11=v3(v11 + ((208 -95) -(v3(v9,67) + v2(v9,67))) ,(2237 -1018) -(v3(v8,v7) + v2(v8,v7)) ) + v2(v11 + ((354 -241) -(v3(v9,85 -(10 + 8) ) + v2(v9,67))) ,(1569 -(87 + 263)) -(v3(v8,v7) + v2(v8,v7)) ) ;local v14=1203636 -(67 + 113) ;local v15=902271 + 328200 ;local v16=v6;if (v15<=v14) then else print("true");end if ((v3(v5,v16) + v2(v5,v16))<=v15) then else local v19=442 -(416 + 26) ;local v20;while true do if (v19==(0 -0)) then v20="Only Lua 5.1 is supported in this platform";print(v20);break;end end end print(v4);do local v17="sieve_of_eratosthenes";function sieve_of_eratosthenes(v21)local v22=4 -2 ;local v23=1 + 0 ;local v24={};for v27=v23,v21 do v24[v27]=1~=v27 ;end for v29=v22,math.floor(math.sqrt(v21)) do if v24[v29] then for v33=v29 * v29 ,v21,v29 do v24[v33]=false;end end end return v24;end local v18=_G[v17](420);for v25,v26 in pairs(v18) do if v26 then local v30=0 -0 ;local v31;local v32;while true do if ((953 -(802 + 150))==v30) then while true do if (v31==0) then v32="Prime found: ";print(v32   .. v25 );break;end end break;end if (v30==(438 -(145 + 293))) then v31=0 -0 ;v32=nil;v30=1 -0 ;end end end end end print("HJK");
